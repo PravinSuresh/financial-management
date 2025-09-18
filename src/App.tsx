be6d7c4be1e69bd0173f65de82dab153eff4e030
Pravin Suresh <selfstudy210619@gmail.com>
2025-09-18 13:04:13 +0530

    future value of lumpsum

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import FutureValueLumpsum from "./components/FutureValueLumpsum";
+
 function App() {
 	return (
 		<>
 			<h1>Financial Wealth Management</h1>
+			<div className='half'>
+				<FutureValueLumpsum />
+			</div>
 		</>
 	);
 }
